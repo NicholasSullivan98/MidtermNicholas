acaa5ac2ee1a577b2cf8baec92ff4778a9d5f0cb
Nicholas Sullivan <sullnich@sheridancollege.ca>
2024-10-17 13:11:01 -0400

    Added Second Page

--- MidtermNicholas/CoffeeOrders.swift
+++ MidtermNicholas/CoffeeOrders.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct CoffeeOrders: View {
+    
+    @Binding var coffeeOrder : Coffee
+    
+    var price : Double
+    var HST : Double
+    var tip : Double = 0
+    var totalPrice : Double
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hi \(coffeeOrder.name) ")
+        Text("Your Order Details:")
+        Text("\(coffeeOrder.size) \(coffeeOrder.type)")
+        Text("Quantity: \(coffeeOrder.numOfCups)")
+        Text("Base Price: \(price)")
+        Text("HST: \(HST)")
+        Text("Tip \(tip)")
+        Text("Total Price: \(totalPrice)")
+        
+    }
+    
+    mutating func priceCalc(){
+        
+        if(coffeeOrder.type == "Original Blend"){
+            price = 3.50
+        }else if(coffeeOrder.type == "Dark Roast"){
+            price = 4.00
+        }else{
+            price = 4.50
+        }
+        
+        if(coffeeOrder.size == "Medium"){
+            price = price + 0.50
+        }else if(coffeeOrder.size == "Large"){
+            price = price + 1.00
+        }
+        
+        HST = price * 0.13
+        
+        if(coffeeOrder.tip == true){
+            tip = 2.00
+            totalPrice = price + HST + tip
+        }else{
+            totalPrice = price + HST
+        }
     }
 }
 
-#Preview {
+/*
+ #Preview {
     CoffeeOrders()
-}
+ }
+ */
+ 
--- MidtermNicholas/ContentView.swift
+++ MidtermNicholas/ContentView.swift
@@ -7,15 +7,86 @@
 
 import SwiftUI
 
+struct Coffee : Identifiable {
+    var id = UUID()
+    var name : String
+    var size : String
+    var type : String
+    var tip : Bool
+    var numOfCups : Int
+}
+
 struct ContentView: View {
+    
+    @State var selectedSize : Size = .Small
+    @State var selectedCoffeeType : CoffeeType = .Original
+    @State var quantity : String = "1"
+    @State var tip = false
+    @State var showAlert  = false
+    @State private var name: String = ""
+
+    @State var coffeeOrder: Coffee = Coffee(name: "", size: "", type: "", tip: false, numOfCups: 0)
+    
+    
+    enum Size: String, CaseIterable, Identifiable {
+        case Small, Medium, Large
+        var id: Self { self }
+    }
+    
+    enum CoffeeType: String, CaseIterable, Identifiable {
+        case  Original, Dark, Vanilla
+        var id: Self { self }
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Text("Coffee Ordering System")
+                    .font(.title)
+                TextField("Enter your Name", text: $name)
+                //Spacer()
+                
+                Text("Select Coffee Type: ")
+                    .padding(5)
+                Picker("Coffee Type", selection: $selectedCoffeeType){
+                    Text("Original Blend").tag(CoffeeType.Original)
+                    Text("Dark Roast").tag(CoffeeType.Dark)
+                    Text("Vanilla").tag(CoffeeType.Vanilla)
+                }.pickerStyle(.segmented)
+                
+                Text("Selected Coffee Size: ")
+                    .padding(5)
+                Picker("Size", selection: $selectedSize){
+                    Text("Small").tag(Size.Small)
+                    Text("Medium").tag(Size.Medium)
+                    Text("Large").tag(Size.Large)
+                }.pickerStyle(.segmented)
+                
+                Text("Would you like to add a $2 Tip?")
+                Toggle(isOn: $tip){
+                    Text("Add Tip")
+                }
+                
+                TextField("Number of Cups", text: $quantity)
+                
+                Button("Add Coffee"){
+                    coffeeOrder.name = name
+                    coffeeOrder.type = selectedCoffeeType.rawValue
+                    coffeeOrder.size = selectedSize.rawValue
+                    coffeeOrder.tip = tip
+                    coffeeOrder.numOfCups = Int(quantity) ?? 0
+                }
+                
+                
+                NavigationLink(destination: CoffeeOrders(coffeeOrder: $coffeeOrder, price: 0, HST: 0, tip: 0, totalPrice: 0)){
+                    Text("Place Order")
+                }.padding(10)
+                
+            }
+            .padding(10)
+        }.alert("Data is not valid", isPresented: $showAlert){
+            Button("Ok" , role: .cancel) {}
         }
-        .padding()
     }
 }
 
