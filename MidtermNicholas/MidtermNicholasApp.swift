b586243cd6466c862cb6d4a09ab7a0b7a00edf10
Nicholas Sullivan <sullnich@sheridancollege.ca>
2024-10-17 11:35:54 -0400

    Initial Commit

---
+++ MidtermNicholas/MidtermNicholasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermNicholasApp.swift
+//  MidtermNicholas
+//
+//  Created by Nicholas Sullivan on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct MidtermNicholasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
